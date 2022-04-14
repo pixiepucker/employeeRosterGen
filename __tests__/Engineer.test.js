12fab1d1cb462aa6b386c77f233938a6918a5b57
pixiepucker <autumnoffic@gmail.com>
2022-04-13 23:39:25 -0500

    commit before moving on to intern/manager

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,8 +1,15 @@
-const { expect } = require('@jest/globals');
+const { expect, test } = require('@jest/globals');
+const Employee = require('../lib/Employee');
 const Engineer = require('../lib/Engineer');
 
 test('see if constructor returns github',()=>{
     const engineer = new Engineer('Dana','2','email2','tacocat');
 
     expect(engineer.github).toBe('tacocat');
+});
+
+test('see if method returns engineer as role', ()=>{
+    const engineer = new Engineer('Dana', '2', 'email2','tacocat');
+
+    expect(engineer.getRole()).toBe('Engineer');
 });
